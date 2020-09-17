41e83154e90399d854816b372861dcd7b0a0fefb
CuiMingyu <mingyuc36@163.com>
2020-09-17 16:32:04 +0800

    Finish CH4.4 Better than yesterday!

--- 00DeepLearning/Ch4/4.4/gradient.py
+++ 00DeepLearning/Ch4/4.4/gradient.py
@@ -33,5 +33,5 @@ def function(x):
     return x[0] ** 2 + x[1] ** 2
 
 
-init_x = np.array([-3.0, 4.0])
+init_x = np.array([-3.0, 5.0])
 print(gradient_descent(function, init_x=init_x, lr=0.1, step_num=100))
